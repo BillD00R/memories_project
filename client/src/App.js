e5a36a5e91d79d1346fb3e73dc4ca2fb7daed3bc
Alexander <soisearach@gmail.com>
2021-03-29 18:02:35 +0300

    updated posts functionality

--- client/src/App.js
+++ client/src/App.js
@@ -22,9 +22,9 @@ const App = () => {
     return (
         <div>
             <Container maxWidth="lg">
-                <AppBar className={classes.AppBar} position="static" color="inherit">
+                <AppBar className={classes.appBar} position="static" color="inherit">
                     <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                    <img className={classes.image} src={memories} alt="memories" height="60" />
+                    <img className={classes.image} src={memories} alt="icon" height="60" />
                 </AppBar>
                 <Grow in>
                     <Container>
