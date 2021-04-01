9f51494e6433650fec1341a834ad5cc16ab550fc
Alexander <soisearach@gmail.com>
2021-04-01 20:59:37 +0300

    final

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -38,3 +38,24 @@ export const updatePost = async (req, res) => {
 
     res.json(updatedPost);
 }
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id = ${id}`);
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.status(204).send(`Post deleted`);
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id = ${_id}`);
+
+    const { likeCount } = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: likeCount + 1 }, { new: true });
+
+    res.json(updatedPost);
+
+};
