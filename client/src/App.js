21635088e71ff304f8cd38e1b4dd66f95af2bee7
Alexander <soisearach@gmail.com>
2021-04-02 14:59:43 +0300

    authorization beginning

--- client/src/App.js
+++ client/src/App.js
@@ -23,10 +23,7 @@ const App = () => {
     return (
         <div>
             <Container maxWidth="lg">
-                <AppBar className={classes.appBar} position="static" color="inherit">
-                    <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                    <img className={classes.image} src={memories} alt="icon" height="60" />
-                </AppBar>
+                
                 <Grow in>
                     <Container>
                         <Grid container justify="space-between" align="items" spacing={3}>
